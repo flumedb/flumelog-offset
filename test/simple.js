f4c98ea1f8511b604a3e606ba1f7937e38e0173f
Dominic Tarr <dominic.tarr@gmail.com>
2016-08-15 23:08:00 +1200

    test getPrevious and stream

--- test/simple.js
+++ test/simple.js
@@ -1,12 +1,21 @@
 
 var tape = require('tape')
-
+var pull = require('pull-stream')
 var Offset = require('../')
 
-tape('simple', function (t) {
 
-  var file = '/tmp/offset-test_'+Date.now()+'.log'
-  var db = Offset(file, 16)
+var file = '/tmp/offset-test_'+Date.now()+'.log'
+var db = Offset(file, 16)
+var live = []
+
+pull(
+  db.stream({live: true, sync: false}),
+  pull.drain(function (data) {
+    live.push(data)
+  })
+)
+
+tape('simple', function (t) {
 
   db.append(new Buffer('hello world'), function (err, offset1) {
     if(err) throw err
@@ -14,15 +23,63 @@ tape('simple', function (t) {
       if(err) throw err
       db.get(offset1, function (err, b) {
         t.equal(b.toString(), 'hello world')
-
         db.get(offset2, function (err, b2) {
           t.equal(b2.toString(), 'hello offset db')
-          t.end()
+          db.getPrevious(offset2, function (err, b) {
+            t.equal(b.toString(), 'hello world')
+            t.end()
+          })
         })
       })
     })
   })
 })
 
+/*
+          8,       4,      32,      32           = 76
+_header = {offset, length, prev_mac, hash(data)}
+        32,           76,      = 108
+header = mac(_header)|_header
+data (length)
+         4
+footer = length
 
+OR, encrypted database?
+
+header_mac (16)
+  [offset(8), length(4), data_mac(16)]
+
+footer_mac(16)
+  [length (4)]
+
+*/
+
+tape('stream', function (t) {
+
+  pull(
+    db.stream({min: 0}),
+    pull.collect(function (err, ary) {
+      t.deepEqual(ary.map(String), ['hello world', 'hello offset db'])
+      t.end()
+    })
+  )
+
+})
+
+tape('live', function (t) {
+  t.deepEqual(live.map(String), ['hello world', 'hello offset db'])
+  t.end()
+})
+
+tape('reverse', function (t) {
+  pull(
+    db.stream({min: 0, reverse: true}),
+    pull.collect(function (err, ary) {
+      t.deepEqual(ary.map(String), ['hello offset db', 'hello world'])
+      t.end()
+    })
+  )
+
+
+})
 
