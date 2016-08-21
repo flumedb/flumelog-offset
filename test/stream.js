14be6c1ce037e6c470224b7e8161b762040e0ade
Dominic Tarr <dominic.tarr@gmail.com>
2016-08-21 22:00:14 +1200

    handle race condition around reading from realtime streams

---
+++ test/stream.js
@@ -0,0 +1,62 @@
+var Log = require('../')
+var pull = require('pull-stream')
+var tape = require('tape')
+
+var log = Log('/tmp/test_offset-log_'+Date.now(), 1024)
+
+function encode (obj) {
+  return new Buffer(JSON.stringify(obj))
+}
+
+function decode (b) {
+  return JSON.parse(b.toString())
+}
+
+tape('append objects, and stream them out the same', function (t) {
+
+  var n = 4
+  var a = [
+    {foo: true, bar: false, r: Math.random()},
+    {foo: true, bar: true, r: Math.random()},
+    {foo: false, bar: true, r: Math.random()},
+    {foo: false, bar: false, r: Math.random()}
+  ]
+
+  var ary = []
+
+  log.append(encode(a[0]), next)
+  log.append(encode(a[1]), next)
+  log.append(encode(a[2]), next)
+  log.append(encode(a[3]), next)
+
+  function next () {
+    if(--n) return
+    pull(
+      log.stream({keys:true, values: true}),
+      pull.map(function (data) {
+        if(data.sync) return data
+        return {key: data.key, value: decode(data.value)}
+      }),
+      pull.collect(function (err, a) {
+        if(err) throw err
+        t.deepEqual(ary, a)
+        t.end()
+      })
+    )
+  }
+
+  pull(
+    log.stream({live: true, keys: true, values: true, sync: false}),
+    pull.map(function (data) {
+      if(data.sync) return data
+      return {key: data.key, value: decode(data.value)}
+    }),
+    pull.drain(function (data) {
+      ary.push(data)
+    })
+  )
+})
+
+
+
+
