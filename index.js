e56a550573ec097a00cda2d3950f0c0b1ba4983e
Dominic Tarr <dominic.tarr@gmail.com>
2016-11-07 14:33:07 +0000

    tidy

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function frame (data) {
     var buf = data[i]
     //mutate the items
     //var buf = item.value
-//    item.offset = 0 + offset
     b.writeUInt32BE(buf.length, 0 + offset) //start
     b.writeUInt32BE(buf.length, 4+buf.length + offset) //end
     buf.copy(b, 4 + offset, 0, buf.length)
@@ -43,44 +42,18 @@ module.exports = function (file, length) {
   var blocks = Blocks(file, length, 'a+')
 
   var append = Append(function (batch, cb) {
-    
     blocks.append(frame(batch), function (err) {
       if(err) return cb(err)
       //else, get offset of last item.
       since.set(blocks.offset.value - (batch[batch.length - 1].length + 8))
-      console.log("APPEND", since.value)
       cb(null, since.value)
     })
-
   })
 
-//  var queue = [], writing = false
-//
-//  function write () {
-//    if(writing) return
-//    if(!queue.length) return
-//    writing = true
-//    var data = []
-//    var framed = frame(queue)
-//    var _queue = queue
-//    queue = []
-//    blocks.append(framed, function (err, _offset) {
-//      writing = false
-//      while(_queue.length) {
-//        var q = _queue.shift()
-//        var o = (_offset - framed.length) + q.offset
-//        q.cb(err, o)
-//      }
-//      //updates since.
-//      if(queue.length) write()
-//    })
-//  }
-//
   var since = Obv()
   var offset = blocks.offset
 
   offset.once(function (offset) {
-    console.log('OFFSET', offset)
     if(offset === 0) return since.set(-1)
     log.getPrevious(offset, function (err, value, length) {
       since.set(offset - length)
@@ -89,15 +62,11 @@ module.exports = function (file, length) {
 
   var log
   return log = {
-//    offset: offset,
     since: since,
     //create a stream between any two records.
     //read the first value, then scan forward or backwards
     //in the direction of the log
 
-    //using pull-live this way means that things added in real-time are buffered
-    //in memory until they are read, that means less predictable memory usage.
-    //instead, we should track the offset we are up to, and wait if necessary.
     stream: function (opts) {
       opts = opts || {}
       var cursor
@@ -144,31 +113,6 @@ module.exports = function (file, length) {
     //if value is an array of buffers, then treat that as a batch.
     append: append,
 
-//function (value, cb) {
-//      //TODO: make this like, actually durable...
-//      if(Array.isArray(value)) {
-//        var offsets = []
-//        value.forEach(function (v) {
-//          queue.push({value: v, cb: function (err, offset) {
-//            offsets.push(offset)
-//            if(offsets.length === value.length) {
-//              for(var i = 0; i < offsets.length; i++)
-//                notify({key: offsets[i], value: value[i]})
-//              cb(null, offsets)
-//            }
-//          }})
-//        })
-//
-//        return write()
-//      }
-//      if(!isBuffer(value)) throw new Error('value must be a buffer')
-//      queue.push({value: value, cb: function (err, _offset) {
-//        if(err) return cb(err)
-//        notify({key: offset, value: value})
-//        cb(null, _offset)
-//      }})
-//      write()
-//    },
     get: function (_offset, cb) {
       if(!isInteger(_offset)) throw new Error('get: offset must be integer')
       //read the block that offset is in.
@@ -199,11 +143,5 @@ module.exports = function (file, length) {
     },
   }
 }
-
-
-
-
-
-
 
 
