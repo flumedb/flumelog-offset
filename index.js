64c38c22df66cd3a917f0b7a8d7426318170a26f
Dominic Tarr <dominic.tarr@gmail.com>
2017-05-19 09:56:01 +1200

    rewrite using pull-cursor, much smaller!

--- index.js
+++ index.js
@@ -8,18 +8,8 @@ var Blocks = require('aligned-block-file')
 var isInteger = Number.isInteger
 var ltgt = require('ltgt')
 var createFrame = require('./frame/basic')
-
-function format (seqs, values, seq, value, cursor) {
-  return (
-    seqs !== false
-    ? values !== false
-      ? {value: value, seq: seq}
-      : seq
-    : value
-  )
-}
-
-var k = 0
+var createStreamCreator = require('pull-cursor')
+var Map = require('pull-stream/throughs/map')
 
 function id (v) { return v }
 var id_codec = {encode: id, decode: id}
@@ -50,13 +40,7 @@ module.exports = function (file, length, codec, cache) {
     })
   })
 
-//  offset.once(function (offset) {
-//    if(offset === 0) return since.set(-1)
-//    log.getPrevious(offset, function (err, value, length) {
-//      since.set(offset - length)
-//    })
-//  })
-//
+  var createStream = require('pull-cursor')(since, frame.getMeta)
 
   frame.restore(function (err, offset) {
     if(err) throw err
@@ -67,177 +51,28 @@ module.exports = function (file, length, codec, cache) {
   return log = {
     filename: file,
     since: since,
-    //create a stream between any two records.
-    //read the first value, then scan forward or backwards
-    //in the direction of the log
-
     stream: function (opts) {
-      opts = opts || {}
-      var cursor
-      var reverse = !!opts.reverse
-      var get = reverse ? log.getPrevious : log.get
-      var diff = reverse ? -1 : 1
-      var live = opts.live
-      var aborted = false
-      var skip = false
+      //note, this syntax means we don't need to import pull-stream.
+      //im not sure about doing encodings like this. seems haphazard.
+      return Map(function (data) {
+        if(Buffer.isBuffer(data)) return codec.decode(data)
+        else if('object' === typeof data)
+          data.value = codec.decode(data.value)
 
-      if(opts.gt == -1) {
-        delete opts.gt
-        opts.gte = 0
-      }
-
-      if(reverse) {
-        if(opts.lt != null) cursor = opts.lt
-        else if(opts.lte != null) {
-          cursor = opts.lte; skip = true
-        }
-      }
-      else {
-        if(opts.gte != null) cursor = opts.gte
-        else if(opts.gt != null) {
-          cursor = opts.gt; skip = true
-        }
-        else cursor = 0
-      }
-
-      var lower = ltgt.lowerBound(opts) || 0
-      var includeLower = ltgt.lowerBoundInclusive(opts)
-      var upper = ltgt.upperBound(opts)
-      var includeUpper = ltgt.upperBoundInclusive(opts)
-
-
-      function next (cb) {
-        if(aborted) return cb(aborted)
-
-        if(!reverse && upper != null && includeUpper ? cursor > upper : cursor >= upper) {
-          return cb(true)
-        }
-
-        //we should not ever get here in live mode.
-        if(!reverse && cursor > since.value) {
-          if(live)
-            return since.once(function () {
-              next(cb)
-            }, false) //throw new Error('live error')
-          return cb(true)
-        }
-
-        get(cursor, function (err, value, length) {
-          if(err) return cb(err)
-          if(!length)
-            throw new Error('read empty value:'+length)
-
-          var _cursor = reverse ? cursor - length : cursor
-          cursor += (length * diff)
-
-          if(reverse && (includeLower ? _cursor < lower : _cursor <= lower)) {
-              return cb(true)
-          }
-
-          if(skip) {
-            skip = false
-            return next(cb)
-          }
-
-          cb(err, format(opts.seqs, opts.values, _cursor, value))
-        })
-      }
-
-      return function (abort, cb) {
-        if(aborted = abort) return cb(abort)
-
-        //uh, should this actually be since.once()?
-        since.once(function (_offset) {
-          //if(_offset < cursor) //throw new Error('offset smaller than cursor')
-          if(cursor == null && reverse) {
-            offset.once(function (_offset) {
-              cursor = _offset; next(cb);
-            })
-          }
-          else if(reverse ? cursor >= 0 : cursor <= _offset) {
-            next(cb)
-          } else {
-            if(!live) return cb(true)
-
-            since.once(function () {
-              next(cb)
-            }, false)
-          }
-        })
-      }
+        return data
+      })
+      (createStream(opts))
     },
-
-//    stream: function (
 
     //if value is an array of buffers, then treat that as a batch.
     append: append,
 
-    getMeta: function (_offset, cb) {
-
-      //special case for first value
-      if(_offset === 0)
-        blocks.readUInt32BE(0, function (err, length) {
-          if(err) return cb(err)
-          cb(null, {
-            seq: 0,
-            value: undefined,
-            next: 0 + length + 8
-          })
-        })
-      else
-        blocks.readUInt32BE(_offset, function (err, len) {
-          if(err) return cb(err)
-
-          //read the length of the previous item.
-          //unless this falls right on the overlap between
-          //two blocks, this will already be in the cache,
-          //so will be just a mem read.
-          blocks.readUInt32BE(_offset - 4, function (err, prev_len) {
-            if(err) return cb(err)
-            cb(null, {
-              seq: _offset,
-              value: undefined,
-              prev: offset - 8 - prev_len,
-              next: _offset + len + 8
-            })
-          })
-        })
-    },
-
-    get: function (_offset, cb) {
-      if(!isInteger(_offset)) throw new Error('get: offset must be integer')
-      //read the block that offset is in.
-      //if offset is near the end of the block, read two blocks.
-      if(_offset == null)
-        throw new Error('get(undefined)')
-
-      blocks.readUInt32BE(_offset, function (err, length) {
-        if(err) return cb(err)
-        blocks.read(_offset + 4, _offset + 4 + length, function (err, value) {
-          if(err) return cb(err)
-          if(value.length !== length) throw new Error('incorrect length, expected:'+length+', was:'+value.length)
-            if(!value.length) return cb(new Error('empty buffer read at:'+_offset))
-            cb(err, codec.decode(value), length + 8)
-        })
+    get: function (offset, cb) {
+      frame.getMeta(offset, function (err, value) {
+        if(err) cb(err)
+        else cb(null, codec.decode(value))
       })
-    },
-    //get the record _before_ the given offset.
-    getPrevious: function (_offset, cb) {
-      //don't read before start of file...
-      if(!isInteger(_offset)) throw new Error('getPrevious: offset must be integer')
-
-      _offset = _offset || blocks.size()
-      if(_offset == 0) return cb(new Error('attempted read previous to first object'))
-      blocks.readUInt32BE(_offset - 4, function (err, length) {
-        if(err) return cb(err)
-        blocks.read(_offset - 4 - length, _offset - 4, function (err, value) {
-          cb(err, codec.decode(value), length + 8)
-        })
-      })
-    },
+    }
   }
 }
-
-
-
 
