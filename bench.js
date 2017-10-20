880a42d1877ebca653b54fb291b8d1cfd1a4ecb3
Dominic Tarr <dominic.tarr@gmail.com>
2017-10-20 23:31:27 +1300

    load bench-flumelog from node_modules not parent dir!

--- bench.js
+++ bench.js
@@ -1,7 +1,7 @@
 var FlumeLog = require('./')
 var codec = require('flumecodec')
 
-require('../bench-flumelog/appendy')(function () {
+require('bench-flumelog')(function () {
   return FlumeLog('/tmp/bench-flumelog-offset' + Date.now(), {
     blockSize: 1024*4,
     codec: codec.json
