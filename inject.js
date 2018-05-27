6f47f7fc5c848b6b79d4c9e7bc4011148470231f
Anders Rune Jensen <arj03@protonmail.ch>
2018-05-27 21:23:07 +0200

    Add method to stream without cache

--- inject.js
+++ inject.js
@@ -30,24 +30,39 @@ module.exports = function (blocks, frame, codec, file, cache) {
   })
 
   function getMeta (offset, cb) {
-    var data = cache.get(offset)
-    if(data) cb(null, data.value, data.prev, data.next)
-    else
-      frame.getMeta(offset, function (err, value, prev, next) {
-        if(err) return cb(err)
+    return getMetaHelper(offset, true, cb)
+  }
 
-        var data = {
-          value: codec.decode(value),
-          prev: prev,
-          next: next
-        }
+  function getMetaNoCache (offset, cb) {
+    return getMetaHelper(offset, false, cb)
+  }
 
-        cache.set(offset, data)
+  function getMetaHelper (offset, useCache, cb) {
+    if (useCache) {
+      var data = cache.get(offset)
+      if (data) {
         cb(null, data.value, data.prev, data.next)
-      })
+        return
+      }
+    }
+
+    frame.getMeta(offset, function (err, value, prev, next) {
+      if(err) return cb(err)
+
+      var data = {
+        value: codec.decode(value),
+        prev: prev,
+        next: next
+      }
+
+      if (useCache)
+        cache.set(offset, data)
+      cb(null, data.value, data.prev, data.next)
+    })
   }
 
   var createStream = createStreamCreator(since, getMeta)
+  var createStreamNoCache = createStreamCreator(since, getMetaNoCache)
 
   frame.restore(function (err, offset) {
     if(err) throw err
@@ -59,6 +74,9 @@ module.exports = function (blocks, frame, codec, file, cache) {
     since: since,
     stream: function (opts) {
       return Looper(createStream(opts))
+    },
+    streamNoCache: function (opts) {
+      return Looper(createStreamNoCache(opts))
     },
 
     //if value is an array of buffers, then treat that as a batch.
