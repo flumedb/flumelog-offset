32ab858c10f7f838f965db9498afd8f2d3dd9544
Anders Rune Jensen <arj03@protonmail.ch>
2018-06-05 22:52:58 +0200

    Better value for blockSize in benchmarks

--- bench.js
+++ bench.js
@@ -3,7 +3,7 @@ var codec = require('flumecodec')
 
 require('bench-flumelog')(function () {
   return FlumeLog('/tmp/bench-flumelog-offset' + Date.now(), {
-    blockSize: 1024*4,
+    blockSize: 1024*16,
     codec: codec.json
   })
 })
